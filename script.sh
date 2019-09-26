c4229f3532bc081fcfe9b785055945a84cca303b
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-26 23:13:51 +0300

    gray

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ npm run build
 npx lerna publish major --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
-git commit -am "package-lock.json update"
+git commit -m "package-lock.json update"
 npm version patch
 git push origin --follow-tags
 
