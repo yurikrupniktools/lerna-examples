a62a401f4ab75a39e8faef96e96bd5906149c224
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-26 22:12:28 +0300

    added amend

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 npm run build
-npx lerna publish major --yes --no-push --conventional-commits
+npx lerna publish major --yes --amend --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -am "package-lock.json update"
